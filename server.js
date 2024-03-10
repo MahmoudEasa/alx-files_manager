a077d5d3f5ff1dd690f9c27dd6926505b72744c8
MahmoudEasa <mu01011422865@gmail.com>
2024-03-10 23:44:00 +0200

    Fix Task 5

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 import express from 'express';
-import bodyParser from 'body-parser';
 import routes from './routes/index';
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(bodyParser.json({ limit: '10mb' }));
+app.use(express.json());
 app.use('/', routes);
 
 app.listen(port, () => {
